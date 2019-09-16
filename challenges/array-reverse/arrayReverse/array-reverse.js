d6fa753f22d56b2f918e9478b9dd55460b24e62a
Angela Vawser <amvawser@gmail.com>
2019-09-16 16:45:17 -0700

    removed push() from code

--- challenges/array-reverse/arrayReverse/array-reverse.js
+++ challenges/array-reverse/arrayReverse/array-reverse.js
@@ -2,12 +2,12 @@ const a = ['rat', 'pig', 'dog', 'rooster', 'monkey', 'goat', 'horse', 'snake', '
 const b = [2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009]
 
 function reverseArray(arr) {
-    let newArr = [];
-    for(let i = arr.length - 1; i >= 0; i--) {
-        newArr.push(arr[i]);
+    for(let i = 0; i < arr.length / 2; i++) {
+        [arr[i], arr[arr.length - i - 1]] = [arr[arr.length - i - 1], arr[i]];
     }
-    return newArr;
+    return arr;
 }
 
 console.log(reverseArray(a));
 console.log(reverseArray(b));
+
