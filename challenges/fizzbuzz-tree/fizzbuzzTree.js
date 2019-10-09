1f28e84beaaee75f216c375e267e2863f63e52a8
Angela Vawser <amvawser@gmail.com>
2019-10-08 21:08:55 -0700

    wrote fizzbuzzTree

--- challenges/fizzbuzz-tree/fizzbuzzTree.js
+++ challenges/fizzbuzz-tree/fizzbuzzTree.js
@@ -1,91 +1,16 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.right = null;
-    this.left = null;
+const fizzbuzzTree = (tree) => {
+  if(tree.value % 3 === 0 && tree.value % 5 === 0) {
+    tree.value = 'FizzBuzz';
   }
-}
-
-class BinaryTree {
-  constructor() {
-    this.root = null;
-    this.traversalArray = [];
+  else if(tree.value % 3 === 0) {
+    tree.value = 'Fizz';
   }
-
-  preOrder(node) {
-    if(node) {
-      this.traversalArray.push(node.value);
-      this.preOrder(node.left);
-      this.preOrder(node.right);
-    }
-    return this.traversalArray;
+  else if(tree.value % 5 === 0) {
+    tree.value = 'Buzz';
   }
-
-  inOrder(node) {
-    if(node) {
-      this.inOrder(node.left);
-      this.traversalArray.push(node.value);
-      this.inOrder(node.right);
-    }
-    return this.traversalArray;
-  }
-
-  postOrder(node) {
-    if(node) {
-      this.postOrder(node.left);
-      this.postOrder(node.right);
-      this.traversalArray.push(node.value);
-    }
-    return this.traversalArray;
-  }
-
-  add(value) {
-    let newNode = new Node(value);
-
-    if(this.root === null) {
-      this.root = newNode;
-    }
-    else {
-      this.addNode(this.root, newNode);
-    }
-  }
-
-  addNode(node, newNode) {
-    if(newNode.value < node.value) {
-      if(node.left === null) {
-        node.left = newNode;
-      }
-      else {
-        this.addNode(node.left, newNode);
-      }
-    }
-    else {
-      if(node.right === null) {
-        node.right = newNode;
-      }
-      else {
-        this.addNode(node.right, newNode);
-      }
-    }
-  }
-
-  contains(value) {
-    let has = false;
-    if(this.value === value) {
-      has = true;
-    }
-    else if(value.left !== null && value < this.value) {
-      this.add(this.left);
-    }
-    else if(this.right !== null && value > this.value) {
-      this.add(this.right);
-    }
-    this.add(this);
-    return has;
-  }
-}
+  return tree.value;
+};
 
 module.exports = {
-  Node,
-  BinaryTree,
+  fizzbuzzTree,
 };
