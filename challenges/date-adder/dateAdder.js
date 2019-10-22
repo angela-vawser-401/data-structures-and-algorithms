de77389dca9f39e13828620181d6f98e3025044c
Angela Vawser <amvawser@gmail.com>
2019-10-22 13:33:00 -0700

    finished code challenge

--- challenges/date-adder/dateAdder.js
+++ challenges/date-adder/dateAdder.js
@@ -1,4 +1,4 @@
-formulaTable = {
+let formulaTable = {
   s: (amount) => amount * 1000,
   m: (amount) => amount * 60 * 1000,
   h: (amount) => amount * 60 * 60 * 1000,
@@ -6,7 +6,7 @@ formulaTable = {
   w: (amount) => amount * 7 * 24 * 60 * 60 * 1000,
   M: (amount) => amount * 4 * 7 * 24 * 60 * 60 * 1000,
   y: (amount) => amount * 12 * 4 * 7 * 24 * 60 * 60 * 1000,
-},
+};
 
 function dateAdder(dateObj, diff) {
   let diffArr = diff.match(/(\d+)(\w)/i);
@@ -14,4 +14,6 @@ function dateAdder(dateObj, diff) {
   let operator = diffArr[2];
   let dateDiff = formulaTable[operator](amount);
   return new Date(dateObj.valueOf() + dateDiff);
-};
+}
+
+console.log(dateAdder(new Date(), '2d'));
