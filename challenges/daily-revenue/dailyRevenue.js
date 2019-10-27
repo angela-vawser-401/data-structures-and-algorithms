40ec70b5bca8dd5eaf5da58bce5faf3bec171f79
Angela Vawser <amvawser@gmail.com>
2019-10-27 12:43:14 -0700

    dailyRevenue function written out...working on tests

--- challenges/daily-revenue/dailyRevenue.js
+++ challenges/daily-revenue/dailyRevenue.js
@@ -1,17 +1,22 @@
-function dateAdder(dateObj, diff) {
-  let result;
-  const diffOperator = diff.slice(diff.length - 1);
-  const diffNumber = Number(diff.slice(0, diff.length - 1));
+function dailyRevenue(transactions) {
 
-  if(diffOperator === 's') result = new Date(dateObj.setSeconds(dateObj.getSeconds() + diffNumber));
-  if(diffOperator === 'm') result = new Date(dateObj.setMinutes(dateObj.getMinutes() + diffNumber));
-  if(diffOperator === 'h') result = new Date(dateObj.setHours(dateObj.getHours() + diffNumber));
-  if(diffOperator === 'd') result = new Date(dateObj.setDate(dateObj.getDate() + diffNumber));
-  if(diffOperator === 'w') result = new Date(dateObj.setDate(dateObj.getDate() + diffNumber * 7));
-  if(diffOperator === 'M') result = new Date(dateObj.setMonth(dateObj.getMonth() + diffNumber));
-  if(diffOperator === 'y') result = new Date(dateObj.setFullYear(dateObj.getFullYear() + diffNumber));
+  let results = {};
 
-  return result;
+  transactions.map(purchase => {
+    const date = purchase.timestamp; 
+    const utcDate = new Date(date)
+      .toUTCString()
+      .split(',')
+      .join('')
+      .substring(0, 15);
+    const dateFormat = new Date(utcDate).toDateString();
+    const val = purchase.price;
+    results[dateFormat] ? results[dateFormat] += val : results[dateFormat] = val;
+  });
+
+  return results;
 }
 
-module.exports = dateAdder;
+module.exports = {
+  dailyRevenue,
+};
